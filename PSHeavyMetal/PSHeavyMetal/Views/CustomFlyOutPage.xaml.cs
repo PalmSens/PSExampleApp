b5b65efc2a452f01ab39f6584a1754c748ad6c8d
dion@muldevelopment.nl <dion@muldevelopment.nl>
2022-05-18 11:20:05 +0200

    Centralized title view label and some polishing

--- PSHeavyMetal/PSHeavyMetal/Views/CustomFlyOutPage.xaml.cs
+++ PSHeavyMetal/PSHeavyMetal/Views/CustomFlyOutPage.xaml.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace PSHeavyMetal.Forms.Views
@@ -19,7 +13,7 @@ namespace PSHeavyMetal.Forms.Views
 
         public override bool ShouldShowToolbarButton()
         {
-            return false;
+            return true;
         }
     }
 }
