b7464e8b91fec37e7d36787b026846a2892724f2
Evert Wiesenekker <contact@wiesweb.nl>
2022-07-12 15:07:54 +0200

    Added Bluetooth support for Android 11 and lower
    Added Jetbrains tooling older to the gitignore file

--- PSHeavyMetal/PSHeavyMetal.Android/Services/PermissionService.cs
+++ PSHeavyMetal/PSHeavyMetal.Android/Services/PermissionService.cs
@@ -12,7 +12,8 @@ namespace PSHeavyMetal.Droid.Services
         public override (string androidPermission, bool isRuntime)[] RequiredPermissions => new List<(string androidPermission, bool isRuntime)>
         {
             (Android.Manifest.Permission.BluetoothScan, true),
-            (Android.Manifest.Permission.BluetoothConnect, true)
+            (Android.Manifest.Permission.BluetoothConnect, true),
+            (Android.Manifest.Permission.AccessFineLocation, true)
         }.ToArray();
     }
 
