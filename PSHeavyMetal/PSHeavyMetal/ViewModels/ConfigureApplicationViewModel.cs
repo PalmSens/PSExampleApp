44455a1e9674ee5d7d3e31a304eeee95d925bb17
dion@muldevelopment.nl <dion@muldevelopment.nl>
2022-07-12 11:07:31 +0200

    Added extra validation when uploading method file

--- PSHeavyMetal/PSHeavyMetal/ViewModels/ConfigureApplicationViewModel.cs
+++ PSHeavyMetal/PSHeavyMetal/ViewModels/ConfigureApplicationViewModel.cs
@@ -3,6 +3,7 @@ using PalmSens.Core.Simplified.XF.Application.Services;
 using PSHeavyMetal.Core.Services;
 using PSHeavyMetal.Forms.Navigation;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using Xamarin.CommunityToolkit.ObjectModel;
@@ -29,12 +30,29 @@ namespace PSHeavyMetal.Forms.ViewModels
 
         private async Task ConfigureMethod()
         {
+            var customFileType =
+                new FilePickerFileType(new Dictionary<DevicePlatform, IEnumerable<string>>
+                {
+                { DevicePlatform.iOS, new[] { "application/octet-stream" } },
+                { DevicePlatform.Android, new[] { "application/octet-stream" } },
+                });
+            var options = new PickOptions
+            {
+                FileTypes = customFileType,
+            };
+
             try
             {
-                var result = await FilePicker.PickAsync();
+                var result = await FilePicker.PickAsync(options);
 
                 if (result != null)
                 {
+                    if (!result.FileName.EndsWith("psmethod"))
+                    {
+                        _messageService.ShortAlert("Please select a valid psmethod file");
+                        return;
+                    }
+
                     using var stream = await result.OpenReadAsync();
 
                     await _appConfigurationService.SaveConfigurationMethod(stream);
